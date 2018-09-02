a8048d66a03a562011bd714a18cd5920ac8e3800
zhourui <zhou7rui@gmail.com>
2018-09-03 00:34:53 +0800

    v4测试dao

---
+++ src/test/java/org/litespring/dao/v4/AccountDao.java
@@ -0,0 +1,10 @@
+package org.litespring.dao.v4;
+
+import org.litespring.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component("accountDao")
+public class AccountDao {
+
+}
---
+++ src/test/java/org/litespring/dao/v4/ItemDao.java
@@ -0,0 +1,7 @@
+package org.litespring.dao.v4;
+
+import org.litespring.stereotype.Component;
+
+@Component("itemDao")
+public class ItemDao {
+}
