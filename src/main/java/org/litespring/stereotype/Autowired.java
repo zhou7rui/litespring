7846347bfe4c47dab73dbb7801cac20ae8b59c19
zhourui <zhou7rui@gmail.com>
2018-09-02 19:41:36 +0800

    添加注解

---
+++ src/main/java/org/litespring/stereotype/Autowired.java
@@ -0,0 +1,12 @@
+package org.litespring.stereotype;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.CONSTRUCTOR,ElementType.FIELD, ElementType.METHOD,ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface Autowired {
+
+    boolean required() default true;
+
+}
